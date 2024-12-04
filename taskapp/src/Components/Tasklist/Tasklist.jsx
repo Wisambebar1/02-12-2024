5e090d1c116058ea58812800eda0742da7dcdc83
Wisambebar1 <wisambebar@gmail.com>
2024-12-04 14:13:32 +0200

    starting to work on useeffect phase, we have done the first part of the useEffect

--- taskapp/src/Components/Tasklist/Tasklist.jsx
+++ taskapp/src/Components/Tasklist/Tasklist.jsx
@@ -1,8 +1,23 @@
 import Task from "../Task/Task"
+import { useState,useEffect } from "react"
 const Tasklist=({tasks})=>{
+    let [newTasks,setNewTasks]=useState([])
+    useEffect(()=>{
+         const fet=async()=>{
+            try{
+                const res=await fetch("https://675041e669dc1669ec1a55ad.mockapi.io/tasks")
+                const dat=await res.json()
+                setNewTasks(dat)
+            } 
+            catch(error){
+                console.log(error)
+            }
+        }
+        fet()
+    },[])
     return(
         <div>
-            {tasks.map((item,index)=>{
+            {newTasks.map((item,index)=>{
                 return(
                 <Task Task={item} key={index}/>
                 )
