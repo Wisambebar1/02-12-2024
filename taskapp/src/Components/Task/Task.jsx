bb7804d88b1530d202d78eafc076861d77ebccca
Wisambebar1 <wisambebar@gmail.com>
2024-12-07 11:07:40 +0200

    changed status, now the update of completed works in mock api

--- taskapp/src/Components/Task/Task.jsx
+++ taskapp/src/Components/Task/Task.jsx
@@ -2,13 +2,34 @@ import {useState} from "react"
 const Task=({Task})=>{
     let [task,settask]=useState(Task)
     let [isCompleted,setIsCompleted]=useState("false")
+    const completeUpdate=async (isCompleted)=>{
+        try{
+            const resp=await fetch(`https://675041e669dc1669ec1a55ad.mockapi.io/tasks/${task.id}`
+                ,{method:"PUT",
+                  headers:{"Content-Type":"application/json"},
+                  body:JSON.stringify({isCompleted:isCompleted})
+                }
+            )
+            if(!resp.ok){
+                throw new Error("failed to update the status of the task")
+            }
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
     const handleClick=()=>{
         if(isCompleted==="true"){
-            setIsCompleted("false")
+              Task.isCompleted="false";
+             setIsCompleted(Task.isCompleted)
+            completeUpdate(Task.isCompleted)
         }
         else{
-            setIsCompleted("true")
+             Task.isCompleted="true";
+             setIsCompleted(Task.isCompleted)
+            completeUpdate(Task.isCompleted)
         }
+        
     }
     const handleDelete=()=>{
         settask(null)
