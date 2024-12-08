4742e4367908405978370d1b7dc85f3734a155b4
Wisambebar1 <wisambebar@gmail.com>
2024-12-08 13:00:26 +0200

    starting to work on context

---
+++ taskapp/src/Components/Appcontext/Appcontext.jsx
@@ -0,0 +1,8 @@
+const Appcontext=()=>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default Appcontext
--- taskapp/src/Components/Tasks/Tasks.jsx
+++
@@ -1,5 +0,0 @@
-const Tasks=[
-    {title:"aa",description:"sss",isCompleted:"yy"},
-    {title:"wow",description:"amazing",isCompleted:"no"}
-]
-export default Tasks
